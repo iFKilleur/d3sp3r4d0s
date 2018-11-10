457e11e6130bba15f35f366182e407e711168b03
iFKilleur <44878843+iFKilleur@users.noreply.github.com>
2018-11-10 16:47:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,12 @@
     });
     
     client.on('message', message => {
-
-        if(message.content === prefix + "Hey"){
-            message.reply(":heartpulse:");
-            console.log("Un utilisateur a effectué la commande d'amour !");
         }
 
 	if(message.content === prefix + "keur"){
-            message.reply("tu es moche va-t-en !");
+            message.channel.send(":heartpulse:");
             console.log("Un utilisateur a effectué la commande de message");
-        }
+        };
 
         if(message.content === prefix + "aide"){
             var help_embed = new Discord.RichEmbed()
